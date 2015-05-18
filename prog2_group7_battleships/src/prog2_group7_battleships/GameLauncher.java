168e256f7021fc112b834c1a4b86ca2777a51266
j.dellsperger <j.dellsperger@mailbox.org>
2015-05-18 16:45:18 +0200

    v1.0 Fixes for ship Placement
    Fixed a bug that caused an indexOutOfBoundException in Board.java upon
    launch.

--- prog2_group7_battleships/src/prog2_group7_battleships/GameLauncher.java
+++ prog2_group7_battleships/src/prog2_group7_battleships/GameLauncher.java
@@ -1,16 +1,23 @@
 package prog2_group7_battleships;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import prog2_group7_battleships.ctrl.Controller;
 import prog2_group7_battleships.ihm.View;
 import prog2_group7_battleships.wrk.Game;
 import prog2_group7_battleships.wrk.GameMode;
 
-public class GameLauncher {
+public class GameLauncher extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         // todo: set gamemode dynamically
         Game game = new Game(GameMode.SINGLE);
         View view = new View();
--- prog2_group7_battleships/src/prog2_group7_battleships/wrk/Board.java
+++ prog2_group7_battleships/src/prog2_group7_battleships/wrk/Board.java
@@ -9,8 +9,8 @@ public class Board {
 
     public Board() {
         this.fields = new Field[BOARD_LENGTH][BOARD_LENGTH];
-        for (int x = 1; x <= BOARD_LENGTH; x++) {
-            for (int y = 1; y <= BOARD_LENGTH; y++) {
+        for (int x = 0; x < BOARD_LENGTH; x++) {
+            for (int y = 0; y < BOARD_LENGTH; y++) {
                 this.fields[x][y] = new Field();
             }
         }
@@ -19,30 +19,29 @@ public class Board {
     boolean placeShip(int xCoordinate, int yCoordinate, Orientation orientation, Ship ship) {
         boolean shipPlaced = false;
         ArrayList<Field> targetedFields = new ArrayList();
-        try {
-            switch (orientation) {
-                case HORIZONTAL:
-                    for (int x = 0; x < ship.getType().getLength(); x++) {
-                        if (!fields[x][yCoordinate].isOccupied()) {
-                            targetedFields.add(fields[x][yCoordinate]);
-                        }
+        switch (orientation) {
+            case HORIZONTAL:
+                for (int x = 0; x < ship.getType().getLength(); x++) {
+                    if (!fields[x][yCoordinate].isOccupied()) {
+                        targetedFields.add(fields[x][yCoordinate]);
                     }
-                    break;
-                case VERTICAL:
-                    for (int y = 0; y < ship.getType().getLength(); y++) {
-                        if (!fields[xCoordinate][y].isOccupied()) {
-                            targetedFields.add(fields[xCoordinate][y]);
-                        }
-                    }
-                    break;
-            }
-            if (targetedFields.size() == ship.getType().getLength()) {
-                for (Field targetedField : targetedFields) {
-                    targetedField.setShip(ship);
                 }
-                shipPlaced = true;
+                break;
+            case VERTICAL:
+                for (int y = 0; y < ship.getType().getLength(); y++) {
+                    if (!fields[xCoordinate][y].isOccupied()) {
+                        targetedFields.add(fields[xCoordinate][y]);
+                    }
+                }
+                break;
+        }
+        System.out.println("targetedFields.size: " + targetedFields.size());
+        System.out.println("ship.type.size: " + ship.getType().getLength());
+        if (targetedFields.size() == ship.getType().getLength()) {
+            for (Field targetedField : targetedFields) {
+                targetedField.setShip(ship);
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
+            shipPlaced = true;
         }
         return shipPlaced;
     }
