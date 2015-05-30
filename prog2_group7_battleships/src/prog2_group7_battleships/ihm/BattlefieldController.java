52b71f6f7d40aed8accf6e8386089d0fe6a8a576
ellr <rog.ellenberger@bluewin.ch>
2015-05-30 14:23:12 +0200

    implemmented placeShip in GUI

--- prog2_group7_battleships/src/prog2_group7_battleships/ihm/BattlefieldController.java
+++ prog2_group7_battleships/src/prog2_group7_battleships/ihm/BattlefieldController.java
@@ -1,47 +1,57 @@
 package prog2_group7_battleships.ihm;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import prog2_group7_battleships.wrk.Board;
 import prog2_group7_battleships.wrk.Field;
-import prog2_group7_battleships.wrk.Game;
 import prog2_group7_battleships.wrk.Ship;
 
 public class BattlefieldController {
 
-    @FXML
-    private GridPane grid;
-
-    private Game game;
-    private Board board;
-
-    public BattlefieldController() {
-    }
-
-    public void initialize(Game game) {
-
-        // this.grid.setCellValueFactory(cellData ->
-        // cellData.getValue().getArtistProperty());
-    }
-
-    public void initClass(Game g) {
-        this.game = g;
-        this.board = this.game.getPlayer1().getBoard();
-    }
-
-    public void fillFields() {
-
-        // TODO if board == null, throw exception
+	@FXML
+    private GridPane gridPlayer;
+	@FXML
+    private GridPane gridOpponent;
+	
+	private GUIView view;
+	
+	@FXML
+	private ComboBox shipTypeSelectionBox;
+	
+	@FXML
+	private RadioButton horizonalRadio;
+	@FXML
+	private RadioButton verticalRadio;
+	
+	private ToggleGroup orientationRadioGroup;
+	
+	
+	public void initialize() {
+		this.orientationRadioGroup = new ToggleGroup();
+	}
+	
+	public void setView (GUIView view) {
+		this.view = view;
+	}
+	
+	
+	public void fillFields(Field[][] boardFields) {
+		
         Rectangle rectField;
 
-        Field[][] boardFields = this.board.getFields();
         Field tempField;
         Ship tempShip;
+        
 
-        for (int x = 0; x < this.board.BOARD_LENGTH; x++) {
-            for (int y = 0; y < this.board.BOARD_LENGTH; y++) {
+        for (int x = 0; x < Board.BOARD_LENGTH; x++) {
+            for (int y = 0; y < Board.BOARD_LENGTH; y++) {
                 tempField = boardFields[x][y];
                 tempShip = tempField.getShip();
                 rectField = new Rectangle();
@@ -49,6 +59,8 @@ public class BattlefieldController {
                 rectField.setWidth(20.0);
                 rectField.setHeight(20.0);
                 rectField.setFill(Color.AQUA);
+                rectField.addEventHandler(MouseEvent.MOUSE_CLICKED, handler);
+                
 
                 if (tempField.isShotAt()) {
                     if (tempShip == null) {
@@ -79,12 +91,30 @@ public class BattlefieldController {
 
                 }
                 rectField.setVisible(true);
-                this.grid.add(rectField, y, x);
+                this.gridPlayer.add(rectField, y, x);
+                
+                EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>() {
+        			@Override
+        			public void handle(MouseEvent event) {
+        				int x = GridPane.getColumnIndex(rectField);
+        				int y = GridPane.getRowIndex(rectField);
+        				handlePlaceShip(x,y);
+        			}
+        		};
             }
         }
-        // TODO check if needed
-        this.grid.setVisible(true);
+	}
 
-    }
+	public void fillFields(Field[][] playerFields, Field[][] opponentFields) {
+		
+	}
 
+	public void handlePlaceShip(int x, int y) {
+		this.view.placeShip(x, y);
+		
+	}
+	
+	public void handleShootShip() {
+		this.view.shootShip();
+	}
 }
---
+++ prog2_group7_battleships/src/prog2_group7_battleships/ihm/BoardController.java
@@ -0,0 +1,18 @@
+package prog2_group7_battleships.ihm;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import prog2_group7_battleships.wrk.Board;
+import prog2_group7_battleships.wrk.Field;
+import prog2_group7_battleships.wrk.Ship;
+
+public class BoardController {
+
+	@FXML
+    private GridPane grid;
+
+	
+	
+}
